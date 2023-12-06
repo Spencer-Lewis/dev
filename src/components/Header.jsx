589e83c3725892317cf250c128d5504eb9352587
Spencer Lewis <spencerlewis987@gmail.com>
2023-12-06 12:08:52 -0500

    Create new Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import CodeIcon from '@mui/icons-material/Code';
 
-const Header = () => (
-  <header className="bg-green-500 text-white text-center py-4">
-    <h1 className="text-3xl font-bold">Spencer Lewis</h1>
-    <p className="text-lg">Full-Stack Developer</p>
-  </header>
-);
+const pages = ['Products', 'Pricing', 'Blog'];
 
-export default Header;
+function ResponsiveAppBar() {
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <CodeIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+            onClick={() => {console.log('Clicked HOME')}}
+          >
+            Home
+          </Typography>
+          <CodeIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pages.map((page) => (
+                <Button
+                    key={page}
+                    onClick={() => {console.log(`Clicked ${page}`)}}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                    {page}
+                </Button>
+                ))}
+            </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
