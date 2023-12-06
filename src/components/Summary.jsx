fcd17759ed35873b2724be58412b7299659e0b1d
Spencer Lewis <spencerlewis987@gmail.com>
2023-12-06 11:49:32 -0500

    Begin creating typed animations for summary page

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { TypeAnimation } from 'react-type-animation';
+import avatar from "../assets/portrait.jpg";
+
 
 const Summary = () => (
     <section id="about" className="container mx-auto my-8 px-4">
-        <h2 className="text-3xl font-bold mb-4">About Me</h2>
+        <Box className="">
+            <Grid container justify="center">
+                <Avatar className="" src={avatar} alt="Spencer Lewis" />
+            </Grid>
+            <Typography className="" variant="h4">
+                <TypeAnimation sequence={["Spencer Lewis"]} typeSpeed={30} />
+            </Typography>
+
+            <Typography className="" variant="h5">
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        "Frontend Developer",
+                        1000,
+                        "Backend Developer",
+                        1000,
+                        "Problem Solver",
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={30}
+                    style={{ fontSize: '2em', display: 'inline-block' }}
+                    repeat={Infinity}
+                />
+            </Typography>
+        </Box>
         <div className="ml-7 bg-white rounded-md p-4">
             <p className="text-lg">
                 Full-Stack Software Engineer with 7 years of experience in front-end, back-end
